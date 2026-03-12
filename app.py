21dc19d4aa4d44658f3c417d94b84fb05ca4149b
renukadagada <renukadagada@gmail.com>
2026-03-12 22:41:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 import streamlit as st
 import joblib
-import numpy as np
 
-# Load model
 model = joblib.load("model.pkl")
 
-st.title("Dementia Detection App")
+st.title("Alzheimer's Detection System")
 
 age = st.number_input("Age")
-education = st.number_input("Education Level")
-memory = st.number_input("Memory Score")
+educ = st.number_input("Education (Years)")
+ses = st.number_input("SES (1-5)")
+mmse = st.number_input("MMSE Score")
+etiv = st.number_input("eTIV")
+nwbv = st.number_input("nWBV")
+asf = st.number_input("ASF")
+gender = st.number_input("Gender (0 = Male, 1 = Female)")
 
 if st.button("Predict"):
-    prediction = model.predict([[age, education, memory]])
-    st.write("Prediction:", prediction)
+    prediction = model.predict([[age, educ, ses, mmse, etiv, nwbv, asf, gender]])
+    st.success(f"Prediction: {prediction[0]}")
