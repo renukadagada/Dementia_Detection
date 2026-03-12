faf3a064b64d20c4bf9a29d17ddc0fbe85ec2b29
renukadagada <renukadagada@gmail.com>
2026-03-12 18:10:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,16 @@
-from flask import Flask, render_template, request
+import streamlit as st
 import joblib
 import numpy as np
 
-app = Flask(__name__)
-
-# Load trained model
+# Load model
 model = joblib.load("model.pkl")
 
-@app.route("/", methods=["GET","POST"])
-def home():
+st.title("Dementia Detection App")
 
-    if request.method == "POST":
+age = st.number_input("Age")
+education = st.number_input("Education Level")
+memory = st.number_input("Memory Score")
 
-        age = float(request.form["age"])
-        educ = float(request.form["educ"])
-        ses = float(request.form["ses"])
-        mmse = float(request.form["mmse"])
-        etiv = float(request.form["etiv"])
-        nwbv = float(request.form["nwbv"])
-        asf = float(request.form["asf"])
-        gender = int(request.form["gender"])
-
-        features = np.array([[age,educ,ses,mmse,etiv,nwbv,asf,gender]])
-
-        prediction = model.predict(features)
-
-        if prediction[0] == 0:
-            result = "Non Demented"
-        elif prediction[0] == 1:
-            result = "Demented"
-        else:
-            result = "Converted"
-
-        return render_template("result.html", prediction=result)
-
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if st.button("Predict"):
+    prediction = model.predict([[age, education, memory]])
+    st.write("Prediction:", prediction)
